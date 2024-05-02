977b20ed3dabb319644286898de8db30b9a5e71d
yousseflach <lachgar.youssef6174@gmail.com>
2024-05-02 16:41:00 +0100

    first commit

---
+++ src/test/java/ma/marjane/digitalisation_processus_recrutement/DigitalisationProcessusRecrutementApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.marjane.digitalisation_processus_recrutement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalisationProcessusRecrutementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
