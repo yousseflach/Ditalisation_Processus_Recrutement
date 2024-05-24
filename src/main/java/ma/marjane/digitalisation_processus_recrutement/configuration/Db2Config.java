cf6c17d2e476cc8437b2ec7e167061b4adcad832
yousseflach <lachgar.youssef6174@gmail.com>
2024-05-24 09:05:57 +0100

    added Db2Configuration

---
+++ src/main/java/ma/marjane/digitalisation_processus_recrutement/configuration/Db2Config.java
@@ -0,0 +1,50 @@
+package ma.marjane.digitalisation_processus_recrutement.configuration;
+
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import jakarta.persistence.EntityManagerFactory;
+
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories(
+        entityManagerFactoryRef = "db2EntityManagerFactory",
+        transactionManagerRef = "db2TransactionManager",
+//        ma/marjane/digitalisation_processus_recrutement
+        basePackages = { "ma.marjane.digitalisation_processus_recrutement.db2.repository" }
+)
+public class Db2Config {
+    @Bean(name="db2DataSource")
+    @ConfigurationProperties(prefix="spring.db2.datasource")
+    public DataSource db2DataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean(name = "db2EntityManagerFactory")
+    public LocalContainerEntityManagerFactoryBean db2EntityManagerFactory(EntityManagerFactoryBuilder builder,
+                                                                          @Qualifier("db2DataSource") DataSource db2DataSource) {
+        return builder
+                .dataSource(db2DataSource)
+                .packages("ma.marjane.digitalisation_processus_recrutement.db2.entity")
+                .build();
+    }
+
+    @Bean(name = "db2TransactionManager")
+    public PlatformTransactionManager db2TransactionManager(
+            @Qualifier("db2EntityManagerFactory") EntityManagerFactory db2EntityManagerFactory) {
+        return new JpaTransactionManager(db2EntityManagerFactory);
+    }
+
+}
